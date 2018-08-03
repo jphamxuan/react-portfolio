72011b3d96e2d22d2ecdd42a1d59aa0af9588967
jphamxuan <34908420+jphamxuan@users.noreply.github.com>
2018-08-03 06:45:19 -0700

    pages setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Landing from './pages/landing/landing'
+import Profile from './pages/profile/profile'
+import Portfolio from './pages/portfolio/portfolio'
+import Contact from './pages/contact/contact'
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+     
+    <Router>
+    <Fragment>
+      <Route exact path='/' component={Landing} />
+      <Route exact path='/profile' component={Profile} />
+      <Route exact path='/portfolio' component={Portfolio} />
+      <Route exact path='/contact' component={Contact} />
+    </Fragment>
+    
+    </Router>
     );
   }
 }
 
 export default App;
+
